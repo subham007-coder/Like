c0864029c879338b80b8848c3118c6c3a49c4138
subham007-coder <dsubham109@gmail.com>
2024-01-06 12:49:26 +0530

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import ludo from './ludo.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    // <App />
-    <ludo />
+    <App />
 )
