484af0e920e57bf157ffe5ed7887b6bc3520b8a2
subham007-coder <dsubham109@gmail.com>
2024-01-06 12:41:28 +0530

    addd

---
+++ src/ludo.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import "./App.css"
+
+function ludo() {
+    return (
+      <>
+        <h1>hii</h1>
+      </>
+    )
+
+}
+
+export default ludo;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import ludo from './ludo.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
